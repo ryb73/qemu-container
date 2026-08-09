434705bd1d080ffc73a6eef81d6f3fa288690118
Ryan <rpbiwer@gmail.com>
2026-08-09 16:31:36 -0400

    nscale model for pi

--- pi.sh
+++ pi.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 source "$(dirname "${BASH_SOURCE[0]}")/vm.env"
 
 echo "==> Building pi image"
-ssh "${SSH_OPTS[@]}" "${SSH_TARGET}" \
-  'podman build -t pi -f -' < "$(dirname "${BASH_SOURCE[0]}")/pi/Containerfile"
+tar -C "$(dirname "${BASH_SOURCE[0]}")/pi" -cf - . | ssh "${SSH_OPTS[@]}" "${SSH_TARGET}" \
+  'podman build -t pi -f Containerfile -'
 
 echo "==> Starting pi image"
 exec ssh -tt "${SSH_OPTS[@]}" "${SSH_TARGET}" "podman run --rm -it pi bash"
